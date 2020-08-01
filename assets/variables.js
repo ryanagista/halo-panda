0eb4834254767cc719551fd10a2d9bacf5d1a4a9
Ryanagista <agistafakturryan@gmail.com>
2020-08-01 09:49:26 +0700

    halo-pana

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["panda", "Sayangku", "Sayang", "kang nyeblak", "Liaku :3"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Jangan luain mas ya",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
